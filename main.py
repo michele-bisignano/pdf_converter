2f74b17fd84a0849424cef33cbb9913048f27af5
BisyB <michele.bisignano2005@gmail.com>
2025-02-04 21:26:28 +0100

    Upload the main definitively

---
+++ main.py
@@ -0,0 +1,15 @@
+# main file
+from pdf_to_exel_converter_commercialisti import pdf_to_exel_converter_commercialisti_function
+from file_path_finder import input_file_path_finder, temp_support_file_path_finder, output_file_path_generator
+
+def main():
+	input_path=input_file_path_finder();
+	temp_support_path= temp_support_file_path_finder();
+	output_path= output_file_path_generator();
+
+	pdf_to_exel_converter_commercialisti_function(input_path, temp_support_path)
+
+	print("\nFile esporato correttamente.\n")
+
+
+main()
