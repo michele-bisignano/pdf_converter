563a0426e07c04749482ae32a5b26265b48f8d8f
BisyB <michele.bisignano2005@gmail.com>
2025-02-04 21:37:35 +0100

    Implement code for general function file

---
+++ generalFunctions.py
@@ -0,0 +1,16 @@
+#This file contains functions that can be useful anywhere.
+
+def find_substring_in_array(array, search_string):
+    search_string = search_string.lower()  # To make the search case-insensitive
+    for index, element in enumerate(array):
+        if element:
+            # Compares if the search_string is contained within the element (case-insensitive)
+            if search_string in element.lower():
+                return index  # Returns the index of the first occurrence found
+    return -1  # Returns -1 if no match is found
+
+
+# Finds the length of the longest row
+def max_row_length(table):    
+    max_length = max(len(row) for row in table)
+    return max_length
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # main file
 from pdf_to_exel_converter import pdf_to_exel_converter_commercialisti_function
-from file_path_finder import input_file_path_finder, temp_support_file_path_finder, output_file_path_generator
+from path_finder import input_file_path_finder, temp_support_file_path_finder, output_file_path_generator
 
 def main():
 	input_path=input_file_path_finder();
